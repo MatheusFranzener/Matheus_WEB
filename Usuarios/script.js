3dba6906cbc84a457d5be605b01c470ee45f4bf3
MatheusFranzener <matheus_hohmann@estudante.sc.senai.br>
2021-11-25 19:38:49 -0300

    novoUpdate

--- Usuarios/script.js
+++ Usuarios/script.js
@@ -1,7 +1,8 @@
-let userName = document.location.search;
-let userName2 = userName.replace('?','');
+let nomeUser = document.location.search;
+nomeUser = nomeUser.replace('?', '');
 
-let avatar = document.createElement('img');
+getUserGithub(nomeUser);
+getUserReposGithub(nomeUser);
 
 function getUserGithub(userName) {
     fetch('https://fake-github.herokuapp.com/api/search/' + userName)
@@ -9,26 +10,57 @@ function getUserGithub(userName) {
             resultado.json().then(function (data) {
                 console.log('User Data:', data);
                 showUserGithub(data);
+                showLoginGithub(data);
+                showAvatarGithub(data);
             });
         }).catch(function (erro) {
             console.log('erro:', erro);
         });
 }
 
-function showUserGithub(user) {
-    if (!user) return;
+function showLoginGithub(userName) {
+    let divLogin = document.createElement('div');
+    divLogin.innerText = userName.login;
+    document.body.appendChild(divLogin);
+}
+
+function showUserGithub(userName) {
     let divName = document.createElement('div');
-    divName.innerText = user.login;
+    divName.innerText = userName.name;
     document.body.appendChild(divName);
+}
+
+function showAvatarGithub(userName) {
+    let Avatar = document.createElement('img');
+    Avatar.src = userName.avatar_url;
+    document.body.appendChild(Avatar);
 }
 
 function getUserReposGithub(userName) {
     fetch('https://fake-github.herokuapp.com/api/search/' + userName + '/repos')
         .then(function (resultado) {
             resultado.json().then(function (data) {
-                console.log('Repositories Data:', data);
+                console.log("Lista: ",data);
+                data.forEach(function (element) {
+                    showNamerepos(element);
+                    showLinkrepos(element)
+                })
+            
             })
         }).catch(function (erro) {
             console.log('erro:', erro);
         });
+}
+
+function showNamerepos(userName) {
+    let name = document.createElement('div');
+    name.innerText = userName.name;
+    document.body.appendChild(name);
+}
+
+function showLinkrepos(userName) {
+    let link = document.createElement('a');
+    link.href = userName.svn_url;
+    link.innerText = userName.svn_url;
+    document.body.appendChild(link);
 }
